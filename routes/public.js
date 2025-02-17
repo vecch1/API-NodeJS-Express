398055e2e3dccd3c6a8ed5a90fba91a5d0f1a950
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2025-02-17 10:37:59 -0300

    prisma / bcrypt

--- routes/public.js
+++ routes/public.js
@@ -1,20 +1,44 @@
 import express, { json } from 'express'
+import bcrypt from 'bcrypt'
+import { PrismaClient } from '@prisma/client'
 
+
+const prisma = new PrismaClient()
 const router = express.Router()
+
 
 // Rota de Cadastro
 
-router.post('/cadastro', (req , res) => {
+router.post('/cadastro',  async (req , res) => {
+    
+    try{
     const user = req.body
+    
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(user.password, salt) 
 
-    res.status(201).json(user)
+    const userDB = await prisma.user.create({
+        data: {
+            email : user.email,
+            name : user.name,
+            password: hashPassword,
+        },
+    })
+
+    res.status(201).json(userDB)
+    }
+    catch(err){
+        console.error("Erro detalhado:", err); // Log do erro completo
+        res.status(500).json({ message: "Erro no servidor, tente novamente" });
+         
+    }
 })
 
 
 export default router
 
 
-// mondodp cluster
+// mongodp cluster
 // name - Lucas
 // Senha - Lvb060306
 // mongodb+srv://Lucas:<db_password>@cluster0.ozdpl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import publicRoutes from './routes/public.js'
 
+
 const app = express()
 app.use(express.json())
 
@@ -8,3 +9,4 @@ app.use('/', publicRoutes)
 
 
 app.listen(8080, () => console.log("Servidor rodando 👌"))
+
