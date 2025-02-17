e2f9b47fa6a84af5e3b9458598a17ea402bef1a4
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2025-02-17 15:43:51 -0300

    login / token JWT

--- routes/public.js
+++ routes/public.js
@@ -1,11 +1,12 @@
 import express, { json } from 'express'
 import bcrypt from 'bcrypt'
 import { PrismaClient } from '@prisma/client'
-
+import jwt from 'jsonwebtoken'
 
 const prisma = new PrismaClient()
 const router = express.Router()
 
+const JWT_SECRET = process.env.JWT_SECRET
 
 // Rota de Cadastro
 
@@ -30,8 +31,44 @@ router.post('/cadastro',  async (req , res) => {
     catch(err){
         console.error("Erro detalhado:", err); // Log do erro completo
         res.status(500).json({ message: "Erro no servidor, tente novamente" });
-         
     }
+})
+
+
+//  Rota de Login
+router.post('/login', async (req,res) => {
+    
+    try{
+        const userInfo = req.body
+
+        //  busca usuario no banco 
+        const user = await prisma.user.findUnique({where: {email: userInfo.email},
+        })
+
+        // verifica se o usuario existe
+        if(!user){
+            return res.status(404).json({message:"usuário não encontrado"})
+        }
+
+
+        // compara as senha do banco com a do usuario digitou
+        const isMatch = await bcrypt.compare(userInfo.password, user.password)
+        
+        if(!isMatch){
+            return res.status(400).json({message: "Senha inválida"})
+        }
+    
+
+        // Gerar o token JWT
+
+
+
+        res.status(200).json(user)
+
+    } catch(err) {
+        res.status(500).json({ message: "Erro no servidor, tente novamente" });
+    }
+
 })
 
 
