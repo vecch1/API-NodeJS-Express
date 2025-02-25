01bddd8f7e65ace3e9ad8d8aa68f80d86645e68b
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2025-02-25 12:02:07 -0300

    login / cors

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ import express from 'express'
 import publicRoutes from './routes/public.js'
 import privateRoutes from './routes/private.js'
 import auth from './middlewares/auth.js'
+import cors from 'cors'
+
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/', publicRoutes)
 app.use('/', auth, privateRoutes)
